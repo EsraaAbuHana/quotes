f6d27140406e86b70e02873c8cc0920f7100d9ff
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-04-13 23:05:39 +0300

    not yet

---
+++ app/src/main/java/quotes/Quotes.java
@@ -0,0 +1,14 @@
+package quotes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quotes {
+String author;
+String text;
+ String[] ArrayOfQuotes;
+ArrayList<String>quotesArray;
+public  Quotes(String author,String text){
+
+}
+}
